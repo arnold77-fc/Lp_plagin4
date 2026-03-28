53f101d1d056f2a078cec33a35be55521764119e
arnold77-fc <arnoldclasic6@gmail.com>
2026-03-28 02:48:18 +0200

    Refactor theme application and menu initialization

--- theme.js
+++ theme.js
@@ -1,7 +1,6 @@
 (function () {
     'use strict';
 
-    // 1. Функція застосування стилів
     function applyTheme(theme) {
         var old = document.getElementById('interface_mod_theme');
         if (old) old.remove();
@@ -31,14 +30,14 @@
         }
     }
 
-    // 2. Створення пункту меню
     function init() {
-        // Реєструємо переклади
+        // 1. Пряма реєстрація перекладів
         Lampa.Lang.add({
             interface_mod_theme_title: { en: 'Interface theme', uk: 'Тема інтерфейсу', ru: 'Тема интерфейса' },
             interface_mod_theme_default: { en: 'Default', uk: 'За замовчуванням', ru: 'По умолчанию' }
         });
 
+        // 2. Створюємо структуру параметра
         var themeItem = {
             title: '{interface_mod_theme_title}',
             type: 'select',
@@ -57,38 +56,45 @@
                 twitch: 'Twitch Dark',
                 apple: 'Apple Glass',
                 hulu: 'Hulu Green'
-            },
-            context: 'interface'
+            }
         };
 
-        // Додаємо в стандартні налаштування
-        Lampa.Settings.add(themeItem);
+        // 3. ДОДАЄМО В МЕНЮ ЧЕРЕЗ СТАНДАРТНИЙ ОБ'ЄКТ (Найнадійніше для Android)
+        Lampa.Settings.main().push({
+            name: 'interface_custom_themes',
+            title: '{interface_mod_theme_title}',
+            icon: '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 3c-4.97 0-9 4.03-9 9s4.03 9 9 9c.83 0 1.5-.67 1.5-1.5 0-.39-.15-.74-.39-1.01-.23-.26-.38-.61-.38-.99 0-.83.67-1.5 1.5-1.5H16c2.76 0 5-2.24 5-5 0-4.42-4.03-8-9-8zm-5.5 9c-.83 0-1.5-.67-1.5-1.5S5.67 9 6.5 9 8 9.67 8 10.5 7.33 12 6.5 12zm3-4C8.67 8 8 7.33 8 6.5S8.67 5 9.5 5s1.5.67 1.5 1.5S10.33 8 9.5 8zm5 0c-.83 0-1.5-.67-1.5-1.5S13.67 5 14.5 5s1.5.67 1.5 1.5S15.33 8 14.5 8zm3 4c-.83 0-1.5-.67-1.5-1.5S16.67 9 17.5 9s1.5.67 1.5 1.5-.67 1.5-1.5 1.5z" fill="currentColor"/></svg>',
+            onOpen: function () {
+                var menu = [];
+                for (var key in themeItem.values) {
+                    menu.push({
+                        title: themeItem.values[key],
+                        value: key,
+                        selected: Lampa.Storage.get('interface_theme_custom', 'default') === key
+                    });
+                }
 
-        // ФОРСОВАНА ПЕРЕВІРКА (для Android 1.12.4)
-        // Якщо зайти в налаштування інтерфейсу, перевіримо чи з'явився пункт
-        Lampa.Listener.follow('settings', function (e) {
-            if (e.type === 'open' && e.name === 'interface') {
-                setTimeout(function() {
-                    if ($('.settings-param[data-name="interface_theme_custom"]').length === 0) {
-                        Lampa.Settings.create(themeItem, e.body);
+                Lampa.Select.show({
+                    title: '{interface_mod_theme_title}',
+                    items: menu,
+                    onSelect: function (item) {
+                        Lampa.Storage.set('interface_theme_custom', item.value);
+                        applyTheme(item.value);
+                        Lampa.Select.hide();
+                    },
+                    onBack: function () {
+                        Lampa.Select.hide();
+                        Lampa.Controller.toggle('settings_component');
                     }
-                }, 50);
+                });
             }
         });
 
-        // Слідкуємо за зміною через Storage
-        Lampa.Storage.listener.follow('change', function (e) {
-            if (e.name === 'interface_theme_custom') {
-                applyTheme(e.value);
-            }
-        });
-
-        // Застосовуємо збережену тему
-        var saved = Lampa.Storage.get('interface_theme_custom', 'default');
-        applyTheme(saved);
+        // 4. Застосування при старті
+        applyTheme(Lampa.Storage.get('interface_theme_custom', 'default'));
     }
 
-    // Запуск після готовності
+    // Запуск
     if (window.appready) init();
     else {
         Lampa.Listener.follow('app', function (e) {
@@ -96,4 +102,3 @@
         });
     }
 })();
-
